f61463fb7e2ae6c12dd6e535028dbb36cd9e7745
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-01 19:45:18 +0200

    all tests passed

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+    if year % 4 == 0 {  // we still have to check if its not divisible by 100
+        if year % 400 == 0 {  // if div by 400, its definitely a leap year
+            return true
+        } else if year % 100 == 0 {  // if div by 100, definitely not a leap y
+            return false
+        }
+
+        return true
+    } else {  // If year not divisible by 4, its definitely not a leap year
+        return false
+    }
 }
