30f6d01bd92758d7426840e8d0cdcc8e0555363f
unknown <dagsagrinbergs@gmail.com>
2020-03-05 19:47:38 +0200

    Change from vector to reversed iterator

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,20 +1,15 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    // Naive solution could be to push input onto a stack structure,
-    // then just pop it off into returnable String]
-
-    // Essentially this takes in &str, splits it into unicode characters
-    // which might aswell be several bytes in length each, and puts them
-    // into a Vec as &str
-    let mut stack = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
+    // Graphemes returns double-ended iterator, so we can
+    // reverse it with rev()
+    let mut iter = input.graphemes(true).rev();
 
     // output String
     let mut output = String::new();
 
-    // Pop chars off in reverse order
-    // The "while let Some" syntax seems very weird to me
-    while let Some(character) = stack.pop() {
+    // Iterate through reversed chars
+    while let Some(character) = iter.next() {
         output.push_str(character);
     }
 
