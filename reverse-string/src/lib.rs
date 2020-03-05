45a6ef002c3aa391f001c1f6a12cdc9c955b4214
unknown <dagsagrinbergs@gmail.com>
2020-03-05 22:25:59 +0200

    collect instead of cycle over iter

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,17 +1,11 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    // Graphemes returns double-ended iterator, so we can
+    // Graphemes returns a double-ended iterator, so we can
     // reverse it with rev()
-    let mut iter = input.graphemes(true).rev();
+    let iter = input.graphemes(true).rev();
 
-    // output String
-    let mut output = String::new();
-
-    // Iterate through reversed chars
-    while let Some(character) = iter.next() {
-        output.push_str(character);
-    }
-
-    output
+    // Iterators can be collected into various collections,
+    // one of which is String
+    iter.collect::<String>()
 }
