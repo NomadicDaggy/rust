cc53325900d7e2c1012adf8404fdf9adadbbf1ac
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-06 18:12:52 +0200

    Un-cancered beer-song

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,41 +1,19 @@
-fn capitalize(s: &str) -> String {
-    let mut c = s.chars();
-    match c.next() {
-        None => String::new(),
-        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
-    }
-}
-
 pub fn verse(n: u32) -> String {
-    let nm = String::from("no more");
-    let bsob = String::from("bottles of beer");
-    let bob = String::from("bottle of beer");
-    let otw = String::from("on the wall");
-    let take = String::from("Take one down and pass it around, ");
-    let store = String::from("Go to the store and buy some more, ");
-    let bow = String::from(" bottles of beer on the wall, ");
-
-    if n == 0 {
-        return format!(
-            "{} {} {}, {} {}.\n{}99 {} {}.\n",
-            capitalize(&nm),
-            &bsob,
-            &otw,
-            &nm,
-            &bsob,
-            &store,
-            &bsob,
-            &otw
-        );
-    } else if n == 1 {
-        return format!("1 {} {}, 1 {}.\nTake it down and pass it around, no more bottles of beer on the wall.\n", &bob, &otw, &bob);
-    } else if 1 < n && n < 99 {
-        return format!("{}{}{}{}.\n{}", n, &bow, n, &bob, &take, (n-1).to_string())
+    match n {
+        0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
+        2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string(),
+        _ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1),
     }
-
-    nm
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    let mut out = String::from(verse(start));
+
+    // Weird that you can't just to (8..6), you have to do (6..8).rev()
+    for n in (end..start).rev() {
+        out = format!("{}\n{}", out, verse(n));
+    }
+
+    out
 }
