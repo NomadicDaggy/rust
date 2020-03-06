73b7c1c78bab85fa1f2cf8350283aa360eb99045
unknown <dagsagrinbergs@gmail.com>
2020-03-06 21:04:33 +0200

    finish proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut string_list = Vec::<String>::new();
+
+    if list.is_empty() {
+        return String::new();
+    }
+
+    for i in 0..list.len() - 1 {
+        // -1 because last line will be special
+        let line = format!("For want of a {} the {} was lost.", list[i], list[i + 1]);
+        string_list.push(line);
+    }
+
+    string_list.push(format!("And all for the want of a {}.", list[0]));
+
+    string_list.join("\n")
 }
