15f1a35727eaf476001b5c36911ba192e84a4957
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-07 12:22:44 +0200

    Finish squares

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,14 +1,14 @@
+// Math from: https://stackoverflow.com/questions/15593428/function-to-calculate-the-difference-between-sum-of-squares-and-square-of-sums
+
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+    (n*(n+1)/2).pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+    n*(n+1)*(2*n+1)/6
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+    //((3*n).pow(2) + 2*n) * (1 - n.pow(2)) / 12
+    square_of_sum(n) - sum_of_squares(n)
 }
