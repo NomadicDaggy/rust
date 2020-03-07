229a150ae9c71d1b63577ceec9fdbddeda096706
unknown <dagsagrinbergs@gmail.com>
2020-03-07 14:38:15 +0200

    Finish gigasecond

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,10 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    // Not entirely sure that I'm allowed to do this as it seems too easy
+    // ...but the exercise did say to check what operations can be performed
+    // on DateTime in the chrono crate. So I take it as a hint to not
+    // re-invent the wheel and use all tools at my disposal :D.
+    start + Duration::seconds(1_000_000_000)
 }
