549dd25964fc5508a4f78c77c79cc5bb7a06f831
unknown <dagsagrinbergs@gmail.com>
2020-03-07 19:07:19 +0200

    just going to another pc

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,11 @@
+/*
+ * Given a number, find the sum of all the unique multiples
+ * of particular numbers up to but not including that number.
+ * If we list all the natural numbers below 20 that are multiples
+ * of 3 or 5, we get 3, 5, 6, 9, 10, 12, 15, and 18.
+ * The sum of these multiples is 78.
+ */
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    
 }
