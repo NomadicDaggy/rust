33d906c1a57d3426b3dd663ef88d6ac0ba461a1b
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-07 20:00:46 +0200

    finish sum of multiples

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -6,6 +6,28 @@
  * The sum of these multiples is 78.
  */
 
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    
+    // HashSet should keep the multiples unique
+    let mut unique_multiples = HashSet::new();
+
+    for factor in factors.iter() {
+        // So we don't divide by 0 next
+        if *factor == 0 {
+            continue;
+        }
+
+        // limit / factor will floor to an integer
+        for multiplier in 1..=(limit / factor) {
+            unique_multiples.insert(factor * multiplier);
+        }
+    }
+
+    // Remove limit if it was added
+    if unique_multiples.contains(&limit) {
+        unique_multiples.remove(&limit);
+    }
+
+    unique_multiples.iter().sum()
 }
