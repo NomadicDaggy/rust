e9945d04e3dc77250f638eae085f5932de271484
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-08 17:28:19 +0200

    finish grains

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,12 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    }
+    2_u64.pow(s - 1) as u64
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    // I can't see any reason not to hardcode it
+    // Otherwise this works: (square(64) - 1) * 2 + 1
+    18_446_744_073_709_551_615_u64
 }
