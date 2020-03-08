c2d99f5435205d86b2deb6ff0017b8d0be5f9b6e
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-08 20:37:20 +0200

    armstrong done with 2 variables

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let digits: Vec<u32> = num
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+    
+    let sum = digits
+        .iter()
+        .map(|d| d.pow(digits.len() as u32))
+        .sum();
+
+    num == sum
 }
