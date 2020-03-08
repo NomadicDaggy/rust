bc9ada667cbb9a3cf765df70a25d20843f59cd4a
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-08 18:48:39 +0200

    Rename for clarity

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,13 +1,13 @@
 // Port from https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
 pub fn factors(n: u64) -> Vec<u64> {
-    let mut out = Vec::<u64>::new();
+    let mut factors = Vec::<u64>::new();
 
     // Make n mutable
     let mut n = n;
 
     // Return 2s until the number is odd
     while n % 2 == 0 {
-        out.push(2_u64);
+        factors.push(2_u64);
         n /= 2;
     }
 
@@ -15,14 +15,14 @@ pub fn factors(n: u64) -> Vec<u64> {
     // range with custom step can be accomplished with "range.step_by(step)"
     for i in (3..=(n as f64).sqrt() as u64).step_by(2) {
         while n % i == 0 {
-            out.push(i as u64);
+            factors.push(i as u64);
             n /= i;
         }
     }
 
     if n > 2 {
-        out.push(n)
+        factors.push(n)
     }
 
-    out
+    factors
 }
