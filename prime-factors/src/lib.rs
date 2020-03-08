7b4d8cb620c0860ff91f93c318a764ebfa9a33b6
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-08 18:47:16 +0200

    Optimized prime-factors

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,47 +1,28 @@
+// Port from https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
 pub fn factors(n: u64) -> Vec<u64> {
-    // Get primes up to n/2
-    let primes = build_sieve(((n / 2) + 1) as usize);
-
     let mut out = Vec::<u64>::new();
+
+    // Make n mutable
     let mut n = n;
 
-    for (idx, is_prime) in primes.iter().enumerate() {
-        if idx == 0 || idx == 1 {
-            continue;
-        }
-        if *is_prime == true {
-            while n % (idx as u64) == 0 {
-                n = n / (idx as u64);
-                out.push(idx as u64);
-            }
-            if n < 2 {
-                return out;
-            }
+    // Return 2s until the number is odd
+    while n % 2 == 0 {
+        out.push(2_u64);
+        n /= 2;
+    }
+
+    // sqrt() is a method only for floats
+    // range with custom step can be accomplished with "range.step_by(step)"
+    for i in (3..=(n as f64).sqrt() as u64).step_by(2) {
+        while n % i == 0 {
+            out.push(i as u64);
+            n /= i;
         }
     }
+
+    if n > 2 {
+        out.push(n)
+    }
+
     out
-}
-
-fn build_sieve(n: usize) -> Vec<bool> {
-    // I'm going to try and implement the Sieve of Eratosthenes
-    // initiallize with n+1 of true
-    let mut sieve = vec![true; n + 1];
-
-    // Compilator complained about indexing with u32, so I changed it here
-    let mut prime = 2_usize;
-
-    // Sieve through
-    while prime * prime <= n {
-        if sieve[prime] == true {
-            // Update multiples of p
-            for i in (prime * prime..n + 1).step_by(prime) {
-                sieve[i] = false;
-            }
-        }
-        prime += 1;
-    }
-
-    // returns boolean array where sieve[i] holds the info:
-    // "Is i-1 a prime"
-    sieve
 }
