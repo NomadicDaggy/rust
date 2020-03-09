0b202ff290191243a8a40458b9f4995bf4893c58
unknown <dagsagrinbergs@gmail.com>
2020-03-09 09:36:40 +0200

    fmt

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,16 +1,14 @@
 pub fn reply(message: &str) -> &str {
-    let has_text = !message.chars().all(char::is_control) && 
-        (
-            message.chars().any(char::is_alphanumeric) || 
-            message.chars().any(|c| char::is_ascii_punctuation(&c))
-        );
+    let has_text = !message.chars().all(char::is_control)
+        && (message.chars().any(char::is_alphanumeric)
+            || message.chars().any(|c| char::is_ascii_punctuation(&c)));
 
     let is_question = message.trim_end_matches(char::is_whitespace).chars().last() == Some('?');
 
     let is_shout =
         (message.to_uppercase() == message) && (message.chars().any(char::is_alphabetic));
 
-    match(has_text, is_question, is_shout) {
+    match (has_text, is_question, is_shout) {
         (true, true, true) => "Calm down, I know what I'm doing!",
         (true, false, true) => "Whoa, chill out!",
         (true, false, _) => "Whatever.",
