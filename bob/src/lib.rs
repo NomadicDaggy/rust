8d3a534b6a52d9b3cacc40560508acce4a0abeb3
unknown <dagsagrinbergs@gmail.com>
2020-03-09 09:24:59 +0200

    Bob working fully, but very convoluted

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,17 +1,34 @@
 pub fn reply(message: &str) -> &str {
-    let question = message.trim().chars().last().unwrap() == '?';
-    let yell = message.to_uppercase() == message;
+    let has_text = !message.chars().all(char::is_control) && 
+        (
+            message.chars().any(char::is_alphanumeric) || 
+            message.chars().any(|c| char::is_ascii_punctuation(&c))
+        );
 
-    if question { 
-        if yell {
-            return "Calm down, I know what I'm doing!";
+    let is_question = message.trim_end_matches(char::is_whitespace).chars().last() == Some('?');
+
+    let is_shout =
+        (message.to_uppercase() == message) && (message.chars().any(char::is_alphabetic));
+
+    if has_text {
+        if is_question {
+            if is_shout {
+                return "Calm down, I know what I'm doing!";
+            } else {
+                return "Sure.";
+            }
         } else {
-            return "Sure.";
+            if is_shout {
+                return "Whoa, chill out!";
+            } else {
+                return "Whatever.";
+            }
         }
     } else {
-        if yell {
-            return "Whoa, chill out!";
+        if is_question {
+            return "Sure.";
         }
     }
-    "Whatever."
+
+    "Fine. Be that way!"
 }
