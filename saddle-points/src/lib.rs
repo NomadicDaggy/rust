bc562266014ac089321e26b094b0167a0e52651b
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-14 21:00:49 +0200

    idontknowwhatimdoing.jpg

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,18 @@
+// "saddle point" is greater than or equal to every element in its row
+// and less than or equal to every element in its column.
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    if input.is_empty() { return Vec::<(usize, usize)>::new() }
+
+    let row_count = input.len();
+    let col_count = input[0].len();
+
+    let mut max_of_rows = Vec::<u32>::new();
+    let mut min_of_cols = Vec::<u32>::new();
+
+    for (row_idx, row) in input.iter().enumerate() {
+        print!("{} {:?} \n", row_idx, row);
+
+    }
+
+    vec![(0, 0)]
 }
