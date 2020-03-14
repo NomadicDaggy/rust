8b74428f7cf6188620d875b97d575c9c6418548b
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-14 15:00:27 +0200

    Finish high-scores

--- high-scores/src/lib.rs
+++ high-scores/src/lib.rs
@@ -1,27 +1,41 @@
-#[derive(Debug)]
-pub struct HighScores;
+use std::cmp::Reverse;
 
-impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        unimplemented!(
-            "Construct a HighScores struct, given the scores: {:?}",
-            scores
-        )
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores: &scores }
     }
 
     pub fn scores(&self) -> &[u32] {
-        unimplemented!("Return all the scores as a slice")
+        self.scores
     }
 
     pub fn latest(&self) -> Option<u32> {
-        unimplemented!("Return the latest (last) score")
+        // cloned() is and iterator, that clones the elements
+        // of its parent iterator.
+        self.scores().last().cloned()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        unimplemented!("Return the highest score")
+        // max() works only on iterators
+        self.scores().iter().max().cloned()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        unimplemented!("Return 3 highest scores")
+        let mut vec = self.scores().to_vec();
+
+        // Weird way to reverse sort
+        vec.sort_by_key(|&num| Reverse(num));
+
+        // Elegant way to cut off at 3rd element, inclusive.
+        // If there are less elements than 3, it just returns all.
+        // Also keeps it a Vec, not slice
+        vec.truncate(3);
+
+        vec
     }
 }
