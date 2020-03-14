f42d823c11b8dffb440889aded25dd933961c437
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-14 17:12:48 +0200

    finish matching brackets

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,6 +1,30 @@
+// algo from: https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-an-expression/
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut stack = Vec::<char>::new();
+
+    let opening_brackets = ['(', '[', '{'];
+    let closing_brackets = [')', ']', '}'];
+
+    for c in string.chars() {
+        if opening_brackets.contains(&c) {
+            stack.push(c);
+        } else if closing_brackets.contains(&c) {
+            let popped = match stack.pop() {
+                Some(n) => n,
+                None => ' ', // This feels dirty
+            };
+
+            let should_be = match c {
+                ')' => '(',
+                ']' => '[',
+                '}' => '{',
+                _ => ' ',
+            };
+
+            if popped != should_be {
+                return false;
+            }
+        }
+    }
+    stack.is_empty()
 }
