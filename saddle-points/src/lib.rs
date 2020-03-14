3450706e78b6f8b24486a725c56255e8f9406b0b
unknown <dagsagrinbergs@gmail.com>
2020-03-14 22:50:32 +0200

    Find idx of max value in each row

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -6,12 +6,25 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     let row_count = input.len();
     let col_count = input[0].len();
 
-    let mut max_of_rows = Vec::<u32>::new();
-    let mut min_of_cols = Vec::<u32>::new();
+    
+    let mut min_of_cols = Vec::<u64>::new();
 
+    // Find max idx of each row (there can be more than one)
     for (row_idx, row) in input.iter().enumerate() {
-        print!("{} {:?} \n", row_idx, row);
+        let mut rowmax_val = 0_u64;
+        let mut rowmax_idxs = Vec::<u64>::new();
 
+        for (i, val) in row.iter().enumerate() {
+            if *val > rowmax_val {
+                rowmax_idxs = Vec::<u64>::new();
+                rowmax_val = *val;
+                rowmax_idxs.push(i as u64);
+            } else if *val == rowmax_val {
+                rowmax_idxs.push(i as u64);
+            }
+        }
+        print!("{} {:?}", row_idx, row);
+        print!("{:?}\n", rowmax_idxs);
     }
 
     vec![(0, 0)]
