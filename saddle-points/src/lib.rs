e100242654950b52df5247cb768c19ccff5669ee
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-03-15 20:19:07 +0200

    Start fresh by finding row maximums

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,58 +1,22 @@
 // "saddle point" is greater than or equal to every element in its row
 // and less than or equal to every element in its column.
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    if input.is_empty() {
-        return Vec::<(usize, usize)>::new();
-    }
-
     let mut res = Vec::<(usize, usize)>::new();
+    
+    if input.is_empty() { return res; }
 
-    // Find max idx of each row (there can be more than one)
+    // For each row, find its maximum value and check if it is also
+    // the values columns minimum.
     for (row_idx, row) in input.iter().enumerate() {
-        let mut rowmax_val = 0_u64;
-        let mut rowmax_idxs = Vec::<u64>::new();
-
-        for (i, val) in row.iter().enumerate() {
-            if *val > rowmax_val {
-                rowmax_idxs = Vec::<u64>::new();
-                rowmax_val = *val;
-                rowmax_idxs.push(i as u64);
-            } else if *val == rowmax_val {
-                rowmax_idxs.push(i as u64);
-            }
-        }
-        // For each maximum row value, see if minimum coincides
-        for i in rowmax_idxs {
-            // If a cell is smaller than this and not in column position i,
-            // then there is no saddle point in this row.
-            // Holds value of row maximum.
-            let value_to_beat = input[row_idx][i as usize];
-            let mut no_saddle_flag = false;
-            let mut saddle_rows = Vec::<usize>::new();
-            // For each row
-            for (idx, row) in input.iter().enumerate() {
-                println!("{:?} {}", row, idx);
-                // we have the right column
-                for (i2, elem) in row.iter().enumerate() {
-                    if i2 == i as usize {
-                        // new minimum found
-                        if *elem < value_to_beat {
-                            no_saddle_flag = true;
-                        } else {
-                            saddle_rows.push(idx);
-                            //res.push((idx, i as usize));
-                        }
-                        println!("i:{} idx:{} flag:{}", i, idx, no_saddle_flag);
-                    }
-                }
-            }
-
-            if !no_saddle_flag {
-                for j in saddle_rows {
-                    res.push((j as usize, i as usize));
-                }
-            }
-        }
+        let row_max_val = row.iter().max().unwrap();
+        let row_max: Vec::<(usize, &u64)> = row
+            .iter()
+            .enumerate()
+            .filter(|(i, x)| *x == row_max_val)
+            .collect();
+        
+        //println!("{:?}", row_max);
     }
+    
     res
 }
