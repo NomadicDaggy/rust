027d27ad62fe3bf36c6864ddc4467a80b803dc9a
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-04-02 20:29:37 +0300

    add some specifications from the tests

--- dot-dsl/src/lib.rs
+++ dot-dsl/src/lib.rs
@@ -1,5 +1,17 @@
+// nodes are strings
+// edges are pairs of strings (tuples)
+// attrs are also pairs of strings but are specified as list of tuples
+// both edges and nodes have attrs
+// graph has with_nodes(), with_edges() and with_attrs()
+// nodes, edges and attrs have is_empty()
+// attrs is a HashMap
+
 pub mod graph {
-    pub struct Graph;
+    pub struct Graph {
+        nodes: Node,
+        edges: Edge,
+        attrs: list,
+    }
 
     impl Graph {
         pub fn new() -> Self {
