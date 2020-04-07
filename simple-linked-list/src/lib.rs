62264257200a1481114fd9c9de5be105fe33118c
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-04-07 12:56:32 +0300

    fmt and clippy

--- simple-linked-list/src/lib.rs
+++ simple-linked-list/src/lib.rs
@@ -12,27 +12,28 @@ struct Node<T> {
 
 impl<T> SimpleLinkedList<T> {
     pub fn new() -> Self {
-        SimpleLinkedList {
-            head: None,
-        }
+        SimpleLinkedList { head: None }
     }
 
     pub fn len(&self) -> usize {
-        if !self.head.is_some() { return 0 }
+        if self.head.is_none() {
+            return 0;
+        }
         let mut this = self.head.as_ref().unwrap().next.as_ref();
         let mut len: usize = 1;
         loop {
-            if !this.is_some() { return len }
+            if this.is_none() {
+                return len;
+            }
             len += 1;
             this = this.unwrap().next.as_ref();
         }
-
     }
 
     pub fn push(&mut self, _element: T) {
         let mut head = None;
         mem::swap(&mut self.head, &mut head);
-        
+
         let n = Node {
             data: _element,
             next: head,
