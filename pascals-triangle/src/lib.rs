33169c08f179ccf4c0002d5633fb72910b82d45f
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-04-10 11:53:08 +0300

    pass no_rows

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -1,11 +1,17 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle {
+    row_count: u32,
+}
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {} rows", row_count);
+        PascalsTriangle {
+            row_count,
+        }
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        let mut out = Vec::new();
+
+        out
     }
 }
