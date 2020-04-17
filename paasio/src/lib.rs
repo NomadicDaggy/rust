dd70e41ca98bb24d4d5082662ef0af86d1ae0431
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-04-17 12:34:40 +0300

    pass first paasio tests

--- paasio/src/lib.rs
+++ paasio/src/lib.rs
@@ -1,32 +1,43 @@
 use std::io::{Read, Result, Write};
+use std::fmt::Debug;
 
 // Apparently this is a tuple struct
-pub struct ReadStats<R>(::std::marker::PhantomData<R>);
+pub struct ReadStats<R> {
+    wrapped: R,
+    reads: usize,
+    bytes_through: usize,
+}
 
 impl<R: Read> ReadStats<R> {
     // _wrapped is ignored because R is not bounded on Debug or Display and therefore
     // can't be passed through format!(). For actual implementation you will likely
     // wish to remove the leading underscore so the variable is not ignored.
-    pub fn new(_wrapped: R) -> ReadStats<R> {
-        ReadStats(std::marker::PhantomData)
+    pub fn new(wrapped: R) -> ReadStats<R> {
+        ReadStats {
+            wrapped,
+            reads: 0,
+            bytes_through: 0
+        }
     }
 
     pub fn get_ref(&self) -> &R {
-        unimplemented!()
+        &self.wrapped
     }
 
     pub fn bytes_through(&self) -> usize {
-        unimplemented!()
+        self.bytes_through
     }
 
     pub fn reads(&self) -> usize {
-        unimplemented!()
+        self.reads
     }
 }
 
-impl<R: Read> Read for ReadStats<R> {
+impl<R: Read + Debug> Read for ReadStats<R> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        unimplemented!("Collect statistics about this call reading {:?}", buf)
+        self.reads += 1;
+        self.wrapped.read(buf)
+            .map(|bt| { self.bytes_through += bt; bt })
     }
 }
 
